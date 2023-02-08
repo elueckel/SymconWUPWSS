ed095e7349d84ede4f38f1804fa1021b3ac6039e
elueckel <30531515+elueckel@users.noreply.github.com>
2023-02-08 04:06:38 +0100

    New version 3.2 Geolocation via object tree

--- WundergroundPWSSync/module.php
+++ WundergroundPWSSync/module.php
@@ -27,6 +27,7 @@ if (!defined('vtBoolean')) {
 			//$this->RegisterPropertyString("Latitude","");
 			//$this->RegisterPropertyString("Longitude","");
 			//$this->RegisterPropertyString("Location", '{"latitude":$this->RegisterPropertyString("Latitude"),"longitude":$this->RegisterPropertyString("Longitude")}');
+			$this->RegisterPropertyBoolean("Manual_Geodata","0");
 			$this->RegisterPropertyString("Location", '{"latitude":0,"longitude":0}');
 			$this->RegisterPropertyInteger("ForecastShort","0");
 			$this->RegisterPropertyBoolean("CalculateUpcomingRain","0");
@@ -126,6 +127,10 @@ if (!defined('vtBoolean')) {
 				$CalculateUpcomingRain = $this->ReadPropertyBoolean("CalculateUpcomingRain");
 
 				//Variablen anlegen
+
+				$vpos = 1;
+				$this->MaintainVariable('man_Longitude', $this->Translate('Longitude'), vtString, "", $vpos++, $this->ReadPropertyBoolean("Manual_Geodata") == true);
+				$this->MaintainVariable('man_Latitude', $this->Translate('Latitude'), vtString, "", $vpos++, $this->ReadPropertyBoolean("Manual_Geodata") == true);
 
 
 				$vpos = 10;
@@ -507,9 +512,19 @@ if (!defined('vtBoolean')) {
 			$WU_ID = $this->ReadPropertyString("WU_ID");
 			$Language = $this->ReadPropertyString("Language");
 			$WU_API = $this->ReadPropertyString("WU_API");
-			$locationObject = json_decode($this->ReadPropertyString('Location'), true);
-			$Latitude = str_replace(",",".",$locationObject['latitude']);
-			$Longitude = str_replace(",",".",$locationObject['longitude']);
+
+			if ($this->ReadPropertyBoolean("Manual_Geodata") == false) {
+				$this->SendDebug('Geolocation: ', 'From Symcon Location Module',0);
+				$locationObject = json_decode($this->ReadPropertyString('Location'), true);
+				$Latitude = str_replace(",",".",$locationObject['latitude']);
+				$Longitude = str_replace(",",".",$locationObject['longitude']);
+			} 
+			else if ($this->ReadPropertyBoolean("Manual_Geodata") == true) {
+				$this->SendDebug('Geolocation: ', 'From Object Tree - Latitude: '.GetValue($this->GetIDForIdent("man_Latitude")).' / Longitude: '.GetValue($this->GetIDForIdent("man_Longitude")),0);
+				$Latitude = str_replace(",",".",GetValue($this->GetIDForIdent("man_Latitude")));
+				$Longitude = str_replace(",",".",GetValue($this->GetIDForIdent("man_Longitude")));
+			}
+
 			$CalculateUpcomingRain = $this->ReadPropertyBoolean("CalculateUpcomingRain");
 
 			$ch = curl_init();
